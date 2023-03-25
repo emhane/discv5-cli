dbc219ce39dfa6361c8213d6d80dcf91cfcf74ce
Emilia Hane <emiliaha95@gmail.com>
2023-03-25 10:09:24 +0100

    fixup! Add passive node behaviour for testing hole punching

--- src/server/services/query.rs
+++ src/server/services/query.rs
@@ -6,29 +6,32 @@ pub async fn run(discv5: Arc<Discv5>, break_time: Duration, search_repetitions: 
     let mut iterations = 0;
     loop {
         log::info!("Searching for peers...");
-        let last_iteration = match search_repetitions {
-            None => false,
+        let run_query = match search_repetitions {
+            None => true,
             Some(search_repetitions) => {
-                iterations += 1;
-                log::info!("...iteration {}/{}", iterations, search_repetitions);
-                search_repetitions == iterations
+                if iterations < search_repetitions {
+                    iterations += 1;
+                    log::info!("...iteration {}/{}", iterations, search_repetitions);
+                    true
+                } else {
+                    false
+                }
             }
         };
         let target_random_node_id = enr::NodeId::random();
         // let unlocked_server = discv5.lock().unwrap();
-        match discv5.find_node(target_random_node_id).await {
-            Err(e) => log::warn!("Find Node result failed: {e:?}"),
-            Ok(found_enrs) => {
-                log::info!("Query Completed. Nodes found: {}", found_enrs.len());
-                for enr in found_enrs {
-                    log::info!("Node: {}", enr.node_id());
+        if run_query {
+            match discv5.find_node(target_random_node_id).await {
+                Err(e) => log::warn!("Find Node result failed: {e:?}"),
+                Ok(found_enrs) => {
+                    log::info!("Query Completed. Nodes found: {}", found_enrs.len());
+                    for enr in found_enrs {
+                        log::info!("Node: {}", enr.node_id());
+                    }
                 }
             }
         }
         log::info!("Connected Peers: {}", discv5.connected_peers());
-        if last_iteration {
-            break;
-        }
         tokio::time::sleep(break_time).await;
     }
 }
